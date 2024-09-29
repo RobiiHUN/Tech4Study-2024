9b2de8982b958582dc33ba7aa6f0b099acad3d7a
RobiiiHUN <otrobesz10@gmail.com>
2024-09-29 13:25:27 +0200

    menu-bugfix

--- js/script.js
+++ js/script.js
@@ -65,3 +65,16 @@ function topFunction() {
         behavior: 'smooth'
     });
 }
+
+
+
+
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
