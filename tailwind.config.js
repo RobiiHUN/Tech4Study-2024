06cc147b7ebd0f7588c4bc296172f1fb8c805f05
PasztorGergo <pasztorg05@gmail.com>
2024-10-04 16:48:57 +0200

    Navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html}"],
+  content: ["./development.html"],
   theme: {
     extend: {
       colors: {
         primary: "#cad748",
         secondary: "#53519e",
+      },
+      fontFamily: {
+        tnr: ["Times New Roman", "sans-serif"],
       },
     },
   },
